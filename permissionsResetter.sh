afcd90245519023bbad72099e575efe7b53f4746
Marshall Ruse <12mr9@queensu.ca>
2016-10-13 09:53:24 -0400

    Update permissionsResetter.sh
    Formatted the output a bit more nicely

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -4,7 +4,8 @@
 ls -l | awk {'print $1 "\t" $9 " " $10'} > currentSettings
 
 if [[ $1 == 's' ]]; then
-        diff -x "currentSettings"  --suppress-common-lines -y currentSettings $2
+        echo -e "Old \t \t Current \t File"
+        diff -x "currentSettings"  --suppress-common-lines -y $2 currentSettings | awk {'print $1 "\t" $4 "\t" $5'}
         rm currentSettings
 fi
 
