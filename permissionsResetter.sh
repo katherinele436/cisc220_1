da2ee9a7171b3646c0bfe3fd504a9e7ab3529d36
Katherine Le <14ttul@queensu.ca>
2016-10-15 18:50:37 -0400

    replace $1 = s by $1 =-s

--- permissionsResetter.sh
+++ permissionsResetter.sh
@@ -3,7 +3,7 @@
 
 ls -l | awk {'print $1 "\t" $9 " " $10'} > currentSettings
 
-if [[ $1 == 's' ]]; then
+if [[ $1 == '-s' ]]; then
         echo -e "Old \t \t Current \t File"
         diff -x "currentSettings"  --suppress-common-lines -y $2 currentSettings | awk {'print $1 "\t" $4 "\t" $5'}
         rm currentSettings
