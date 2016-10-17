77e101eb60d8b570510171686c3b6258546f82e1
hw66 <hw66@live.ca>
2016-10-17 10:53:30 -0400

    Q1

--- calculator.sh
+++ calculator.sh
@@ -10,38 +10,58 @@ while true; do
 done
 
 read -p "Please enter a number: " num1                                    # Lets user choose 1st number to begin the operation
+if [[ "$num1" != [0-9] ]]; then
+   read -p "An error has occurred, please enter a valid number: " num1    # Prompts user to choose valid number if error occurs
+else
+   while true; do
+  
+      read -p "Please select an operation (+, -, * or /): " op            # Allows user to unde
+  
+      if [ $op == '+' ]; then                                             # Addition option for the calculator
+         read -p "Please enter a number: " num2
+         if [[ "$num1" != [0-9] ]]; then
+            read -p "An error has occurred, please enter a valid number: " num2   
+         else 
+            total=(($num1 + $num2))
+            echo The total so far is $total
+         fi
+  
+      elif [ $op == '-' ]; then                                            # Substraction option for the calculator
+         read -p "Please enter a number: " num2
+         if [[ "$num1" != [0-9] ]]; then
+            read -p "An error has occurred, please enter a valid number: " num2   
+         else 
+            total=(($num1 - $num2))
+            echo The total so far is $total
+         fi
+  
+      elif [ $op == '*' ]; then                                            # Multiplication option for the calculator
+         read -p "Please enter a number: " num2
+         if [[ "$num1" != [0-9] ]]; then
+            read -p "An error has occurred, please enter a valid number: " num2   
+         else 
+            total=(($num1 * $num2))
+            echo The total so far is $total
+         fi
+  
+      elif [ $op == '/' ]; then                                             # Division option for the calculator
+         read -p "Please enter a number: " num2
+         if [[ "$num1" != [0-9] ]]; then
+            read -p "An error has occurred, please enter a valid number: " num2   
+         else 
+            total=(($num1 / $num2))
+            echo The total so far is $total
+         fi
+  
+      elif [ $op != '+' && $op != '-' && $op != '*' && $op != '/' ]; then     # Prompts user for valid operation if invalid one is chosen
+         read -p "An error has occured; please enter a valid operation: " op
+  
+      else [ $op == ':q']; then                                               # If ':q' is entered, the loop exits and the script closes
+         echo The calculator will now close
+         break
+  
+      fi
 
-while true; do
-  
-  read -p "Please select an operation (+, -, * or /): " op                # Allows user to unde
-  
-  if [ $op == '+' ]; then                                                 # Addition option for the calculator
-    read -p "Please enter a number: " num2
-    total = 'expr $num1 + $num2'
-    echo The total so far is $total
-  
-  elif [ $op == '-' ]; then                                               # Substraction option for the calculator
-    read -p "Please enter a number: " num2
-    total = 'expr $num1 - $num2'
-    echo The total so far is $total
-  
-  elif [ $op == '*' ]; then                                               # Multiplication option for the calculator
-    read -p "Please enter a number: " num2
-    total = 'expr $num1 * $num2'
-    echo The total so far is $total
-  
-  elif [ $op == '/' ]; then                                               # Division option for the calculator
-    read -p "Please enter a number: " num2
-    total = 'expr $num1 / $num2'
-    echo The total so far is $total
-  
-  elif [ $op != '+' && $op != '-' && $op != '*' && $op != '/' ]; then     # Prompts user for valid operation if invalid one is chosen
-    read -p "An error has occured; please enter a valid operation: " op
-  
-  else [ $op == ':q']; then                                               # If ':q' is entered, the loop exits and the script closes
-    echo The calculator will now exit
-    break
-  
-  fi
+fi
 
 done
